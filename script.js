a072c77adb3baf5ce46a2f45ff0693a1961bbf5e
Rafaela Guedes da Costa <rafaela.costa@meta.com.br>
2022-11-09 20:54:26 -0300

    exercicios-aula

---
+++ script.js
@@ -0,0 +1,49 @@
+/*
+const booleano1 = true
+const booleano2 = false
+const booleano3 = true
+
+if(booleano1){
+    alert("Entrou no if")
+}else{
+    alert("Entrou no else")
+}
+
+if(booleano1 == booleano2){
+    alert("1 e 2 são iguais!")
+    }
+else 
+    if(booleano2 == booleano3){
+        alert("2 e 3 são iguais")
+    }
+else  
+    if(booleano1 == booleano3){
+        alert("1 e 3 são iguais")
+    }
+else{
+    alert("Os valores são diferentes!")
+}
+*/
+/*
+const idade = 10
+
+if(idade >= 18 & idade < 60){
+    alert("Pode emitir o título")
+}else if(idade < 15){
+    alert("Não pode emitir o título")
+}else if(idade >= 60){
+    alert("Caso facultativo")
+}else if(idade >= 16 & idade<=17){
+    alert("Caso facultativo, menor idade")
+}
+*/
+
+const media = 10
+
+if(media>=5){
+    console.log("Aprovada")
+}else if (media>=3 && media<5){
+    console.log("Recuperação")
+}else if (media<3){
+    console.log("Reprovada")
+}
